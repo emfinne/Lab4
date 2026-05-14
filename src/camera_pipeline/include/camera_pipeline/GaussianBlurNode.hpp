a7ade15d446f7c8775105633f5d24012bda92f99
Emil <emfinne13@gmail.com>
2026-05-14 10:49:17 +0200

    assss

--- src/camera_pipeline/include/camera_pipeline/GaussianBlurNode.hpp
+++ src/camera_pipeline/include/camera_pipeline/GaussianBlurNode.hpp
@@ -1,8 +1,21 @@
-//
-// Created by parallels on 5/14/26.
-//
 
 #ifndef LAB4_GAUSSIANBLURNODE_HPP
 #define LAB4_GAUSSIANBLURNODE_HPP
+
+
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "cv_bridge/cv_bridge.h"
+#include <opencv2/opencv.hpp>
+class GaussianBlurNode  : public rclcpp::Node
+{
+    public:
+    GaussianBlurNode();
+
+
+
+    private:
+};
 
 #endif //LAB4_GAUSSIANBLURNODE_HPP
--- src/camera_pipeline/src/GaussianBlurNode.cpp
+++ src/camera_pipeline/src/GaussianBlurNode.cpp
@@ -1,3 +1,25 @@
 //
 // Created by parallels on 5/14/26.
 //
+
+
+
+int main(int argc, char **argv) {
+
+    rclcpp::init(argc, argv);
+
+
+        auto gaus_node = std::make_shared<GaussianBlurNode>();
+
+
+        rclcpp::executors::SingleThreadedExecutor executor;
+        executor.add_node(node);
+        std::thread spinner([&executor]() { executor.spin(); });
+
+        executor.add_node(coordinator_node);
+        executor.spin();
+
+
+    rclcpp::shutdown();
+    return 0;
+}
